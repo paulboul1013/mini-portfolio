b30f1ef731f8a7610c734a4b5e23d17fd6d1abaa
paulboul1013 <paulboul1013@gmail.com>
2026-05-21 20:10:10 +0800

    Improve contact alert message

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const contactBtn = document.querySelector("#contactBtn");
 
 contactBtn.addEventListener("click", () => {
-  alert("Thanks for visiting my portfolio!");
+  alert("Thanks for visiting! You can contact me via GitHub.");
 });
